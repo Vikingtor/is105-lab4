d60d5d8e727e8060417645febec3414dc6732a5c
Vikingtor <vikhokjoh@gmail.com>
2021-03-19 19:59:15 +0100

    Oppdaterer repositry

--- readfile.c
+++ readfile.c
@@ -2,15 +2,24 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-int main() {
-    int fd, sz;
-    char *buf = (char *) calloc(120, sizeof(char));
-    fd = open("foo.txt", O_RDONLY); 
-    if (fd < 0) {
-       perror("feil ved lesing af fil"); exit(1);
-    }
-    sz = read(fd, buf, 120);
-    printf("Gjorde et kall til read(%d, buf, 120). Returnerte at %d bytes ble lest.\n", fd, sz);
-    printf("Bytes som ble lest var: %s\n", buf);
-    close(fd);
+int main()// Main class of type int
+{
+	// Parameters of type int
+	int fd, sz;
+	// Char stores characters and letters,
+        // we allocate 300 bytes.
+	char *buf = (char *) calloc(300, sizeof(char));
+	// Opens foo.txt and stores it's content in parameter fd
+	fd = open("foo.txt", O_RDONLY);
+	// If content of fd is less then 0 (there is no content)
+	if (fd < 0)
+	{
+		// Returns an error message
+		perror("feil ved lesing af fil"); exit(1);
+	}
+	// 
+	sz = read(fd, buf, 300);
+	printf("Gjorde et kall til read(%d, buf, 300). Returnerte at %d bytes ble lest.\n", fd, sz);
+	printf("Bytes som ble lest var: %s\n", buf);
+	close(fd);
 }
