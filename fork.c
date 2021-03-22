cb3a9504e842ae78508522c845369ccb8cd539cc
Vikingtor <vikhokjoh@gmail.com>
2021-03-23 00:11:31 +0100

    Made some changes, lab 4 is complete

--- fork.c
+++ fork.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main()
 {
+fork();
+fork();
+fork();
+fork();
 	printf("Tester 4 fork! ");
-	fork();
-	fork();
-	fork();
-	fork();
+	// fork();
+	// fork();
+	// fork();
+	// fork();
 }
